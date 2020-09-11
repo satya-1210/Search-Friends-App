faf780a55254cfa9b79c584cb3061186e0885ebb
satya-1210 <satya.1210@gmail.com>
2020-09-11 15:16:50 +0530

    Updates

---
+++ precache-manifest.76609a100cab3b41fc86135bea2e2be8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c182e4d971ad54a96560d6f65f4777",
+    "url": "/Search-Friends-App/index.html"
+  },
+  {
+    "revision": "c23806758cb658f1b7bc",
+    "url": "/Search-Friends-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a9a1244321f7311ef5d0",
+    "url": "/Search-Friends-App/static/css/main.4919596d.chunk.css"
+  },
+  {
+    "revision": "c23806758cb658f1b7bc",
+    "url": "/Search-Friends-App/static/js/2.4b02dd3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Search-Friends-App/static/js/2.4b02dd3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a1244321f7311ef5d0",
+    "url": "/Search-Friends-App/static/js/main.f8f37678.chunk.js"
+  },
+  {
+    "revision": "0f5a3002de583a2ded61",
+    "url": "/Search-Friends-App/static/js/runtime-main.9b93962a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Search-Friends-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
